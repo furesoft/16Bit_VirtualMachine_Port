ebe0cd7fe54b4b867ae83d0c7ee78a2243a66b69
Chris Anders <filmee24@gmail.com>
2020-05-23 13:15:28 +0200

    Inital Solution Commit

---
+++ Source/Assembler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assembler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Source/VmRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VmRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
