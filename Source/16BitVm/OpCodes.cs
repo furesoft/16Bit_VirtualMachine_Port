28c73992662f6e9cb6e703e4ae5054206b163450
Chris Anders <filmee24@gmail.com>
2020-05-24 10:25:40 +0200

    added new opcodes

--- Source/16BitVm/OpCodes.cs
+++ Source/16BitVm/OpCodes.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace BitVm.Lib
+﻿namespace BitVm.Lib
 {
     public enum OpCodes : byte
     {
@@ -7,7 +6,17 @@ namespace BitVm.Lib
         MOV_REG_REG = 0x11,
         MOV_REG_MEM = 0x12,
         MOV_MEM_REG = 0x13,
+
         ADD_REG_REG = 0x14,
+
         JMP_NOT_EQ = 0x15,
+
+        PSH_LIT = 0x17,
+        PSH_REG = 0x18,
+        POP = 0x1A,
+
+        CAL_LIT = 0x5E,
+        CAL_REG = 0x5F,
+        RET = 0x60,
     }
 }
