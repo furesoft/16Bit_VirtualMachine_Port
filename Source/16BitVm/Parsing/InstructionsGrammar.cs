f472af32b70e6df088d8e05c337e5b4384da8242
Chris Anders <filmee24@gmail.com>
2020-05-27 12:04:02 +0200

    added move parser

---
+++ Source/16BitVm/Parsing/InstructionsGrammar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BitVm.Lib.Parsing.AST;
+using Sprache;
+
+namespace BitVm.Lib.Parsing
+{
+    public static class InstructionsGrammar
+    {
+       public static Parser<ISyntaxNode> Mov()
+        {
+            const string mnemonic = "mov";
+
+            return AsmInstructionBaseGrammar.RegReg(mnemonic).
+                Or(AsmInstructionBaseGrammar.LitReg(mnemonic)).
+                Or(AsmInstructionBaseGrammar.MemReg(mnemonic)).
+                Or(AsmInstructionBaseGrammar.RegMem(mnemonic)).
+                Or(AsmInstructionBaseGrammar.LitMem(mnemonic)).
+                Or(AsmInstructionBaseGrammar.RegPtrReg(mnemonic).
+                Or(AsmInstructionBaseGrammar.LitOffReg(mnemonic)));
+        }
+    }
+}
