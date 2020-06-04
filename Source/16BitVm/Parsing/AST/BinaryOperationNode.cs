8d66a7e7ecbab35c8cfcfa4aaccfc82632241378
Chris Anders <filmee24@gmail.com>
2020-06-04 20:49:12 +0200

    added BinaryOperationNode to ast

---
+++ Source/16BitVm/Parsing/AST/BinaryOperationNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BitVm.Lib.Parsing.AST
+{
+    public class BinaryOperationNode : ISyntaxNode
+    {
+        public BinaryOperationNode(ISyntaxNode left, Operators @operator, ISyntaxNode right)
+        {
+            Left = left;
+            Operator = @operator;
+            Right = right;
+        }
+
+        public ISyntaxNode Left { get; set; }
+        public Operators Operator { get; set; }
+        public ISyntaxNode Right { get; set; }
+
+        public void Accept(SyntaxNodeVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
