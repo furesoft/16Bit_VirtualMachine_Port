da637a94c43e68581ee7d25965da5e8c1daaab84
Chris Anders <filmee24@gmail.com>
2020-06-05 11:19:52 +0200

    cleanup

--- Source/VmRunner/Program.cs
+++ Source/VmRunner/Program.cs
@@ -10,6 +10,9 @@ namespace VmRunner
     {
         static void Main(string[] args)
         {
+            var sg = new AsmPrimitiveGrammar();
+            var vvv = sg.SquareBracketExpression.Parse("[eax - 4]");
+
             var program = new byte[] {
                 (byte)OpCodes.MOV_LIT_REG, 65, 0xFF, (byte)Registers.R5, // write A
                 (byte)OpCodes.MOV_REG_MEM, (byte)Registers.R5, 0xff,0,
