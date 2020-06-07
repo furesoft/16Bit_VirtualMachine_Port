9d798c831e81cf19e3d1e99b54016011212d97cb
Chris Anders <filmee24@gmail.com>
2020-06-07 09:35:44 +0200

    added enum for instruction size

---
+++ Source/16BitVm/InstructionTypeSizes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace BitVm.Lib
+{
+    public enum InstructionTypeSizes : byte
+    {
+        NoArg = 1,
+        LitReg = 4,
+        RegLit = 4,
+        RegLit8 = 3,
+        RegReg = 3,
+        RegMem = 4,
+        MemReg = 4,
+        LitMem = 5,
+        RegPtrReg = 3,
+        LitOffReg = 5,
+        SingleReg = 2,
+        SingleLit = 3,
+    }
+}
