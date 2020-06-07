e9df5dc5f26f136b9f3d26d95a29dd6f2448b271
Chris Anders <filmee24@gmail.com>
2020-06-07 11:53:44 +0200

    added sizetable

---
+++ Source/16BitVm/SizeTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BitVm.Lib
+{
+    public static class SizeTable
+    {
+        private static Dictionary<OpCodes, InstructionTypeSizes> _entries = new Dictionary<OpCodes, InstructionTypeSizes>();
+
+        public static void Init()
+        {
+            var types = Assembly.GetCallingAssembly().GetTypes().Where(_ => _.GetInterfaces().Contains(typeof(IInstruction)));
+            foreach (var t in types)
+            {
+                var instance = (IInstruction)Activator.CreateInstance(t);
+
+                _entries.Add(instance.Instruction, instance.Size);
+            }
+        }
+
+        public static InstructionTypeSizes GetEntry(OpCodes op)
+        {
+            return _entries[op];
+        }
+    }
+}
