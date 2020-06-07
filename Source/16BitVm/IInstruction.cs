a28b8c7ee0a6d71e17b02ee7ef3a9cdd8c296e16
Chris Anders <filmee24@gmail.com>
2020-06-07 09:36:17 +0200

    cleanup

--- Source/16BitVm/IInstruction.cs
+++ Source/16BitVm/IInstruction.cs
@@ -3,6 +3,7 @@ namespace BitVm.Lib
 {
     public interface IInstruction
     {
+        InstructionTypeSizes Size { get; }
         OpCodes Instruction { get; }
         bool Invoke(CPU cpu);
     }
