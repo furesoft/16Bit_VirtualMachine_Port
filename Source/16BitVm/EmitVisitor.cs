c25cae28294295dae5ec858e72baa0e8bafe9289
Chris Anders <filmee24@gmail.com>
2020-06-07 11:53:38 +0200

    added emitvisitor

---
+++ Source/16BitVm/EmitVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using BitVm.Lib.Parsing.AST;
+
+namespace BitVm.Lib
+{
+    public class EmitVisitor : SyntaxNodeVisitor
+    {
+        public Dictionary<string, int> Labels = new Dictionary<string, int>();
+        private int _currentAddress;
+        private Emitter _emitter = new Emitter();
+
+        public override void Visit(CompilationUnit cu)
+        {
+            foreach (var c in cu.Children)
+            {
+                if(c is LabelNode ln)
+                {
+                    Labels.Add(ln.Name, _currentAddress);
+                }
+                else if(c is InstructionNode node)
+                {
+                    _emitter.EmitInstruction(node);
+                    _currentAddress += node.GetTypeInfo().ToString()
+                }
+            }
+        }
+    }
+}
