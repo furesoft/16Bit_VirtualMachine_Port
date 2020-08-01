3575a858bf81dc2b978d3e67616b2fc6d2881ec8
Chris Anders <filmee24@gmail.com>
2020-08-01 10:47:40 +0200

    Cleanup

--- Source/VmRunner/Program.cs
+++ Source/VmRunner/Program.cs
@@ -9,17 +9,6 @@ namespace VmRunner
     { 
         static void Main(string[] args)
         {
-            string[] example = {
-                "my_label:",
-            "mov $4200, &my_label",
-            "mov r1, &0060",
-            "mov $1300, r1",
-            "mov &0060, r2",
-            "add r1, r2",
-              };
-
-            var iss = Grammar.Parse(string.Join('\n', example)); 
-
             var program = new byte[] {
                 (byte)OpCodes.MOV_LIT_REG, 65, 0xFF, (byte)Registers.R5, // write A
                 (byte)OpCodes.MOV_REG_MEM, (byte)Registers.R5, 0xff,0,
@@ -30,17 +19,10 @@ namespace VmRunner
              };
 
             SizeTable.Init();
-            EmitVisitor visitor = new EmitVisitor();
 
-            Emitter em = new Emitter(visitor);
-            visitor.Visit(iss);
-
-            var output = em.ToArray();
-            var strOutp = string.Join(' ', output);
-            //var vvv = sg.SquareBracketExpression.Parse("[eax - 4]");
-            
-            MemoryMapper.Map(new MemoryBankedDevice(8), 0, 0xff);
-            MemoryMapper.Map(new MemoryDevice(), 0xff, 0xffff, true);
+            MemoryMapper.Map(new RegisterMemoryDevice(), 0, 30, true);
+            MemoryMapper.Map(new MemoryBankedDevice(4), 64, 0xff+31, true);
+            MemoryMapper.Map(new MemoryDevice(), 0xff+61, 0xffff, true);
 
             // Map 0xFF bytes of the address space to an "output device" - just stdout
             MemoryMapper.Map(new ScreenDevice(), 0xff, 0x30ff, true);
